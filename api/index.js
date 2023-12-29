f7f77ce6231af81f998204da0eb7a738a1b165f5
vikram110703 <sainivks4321@gmail.com>
2023-12-29 20:49:08 +0530

    cors

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use(cors(
     "routes": [
       {
         "headers": {
-          "Access-Control-Allow-Origin": [`${process.env.FRONTEND_URL}`, "http://localhost:5173"]
+          "Access-Control-Allow-Origin": [`${process.env.Frontend_URL}`, "http://localhost:5173"]
         }
       }
     ]
